4685f2726ff9bdfe4e94827ab022cb078afa5acb
jobsicke <doh292929@gmail.com>
2024-12-26 17:16:20 +0900

    edit

--- src/components/gnb.tsx
+++ src/components/gnb.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import styles from "../styles/gnb.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import SNB from "@/components/snb"
+import SNB from "./snb"
 
 const GNB: React.FC = () => {
     const [isSnbOpen, setIsSnbOpen] = useState(false);
