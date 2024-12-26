94381cca51d170ababc9fc4545f753c976dfe3d4
jobsicke <doh292929@gmail.com>
2024-12-26 18:53:00 +0900

    service

--- src/components/fnb.tsx
+++ src/components/fnb.tsx
@@ -8,8 +8,8 @@ const FNB: React.FC = () => {
     <div className={styles.fnb}>
       <div className={styles.links}>
         <Link href="https://nextomo.vercel.app/">NEXTOMO</Link>
-        <Link href="/">개인정보처리방침</Link>
-        <Link href="/">이용약관</Link>
+        <Link href="/services/terms">개인정보처리방침</Link>
+        <Link href="/services/privacy">이용약관</Link>
       </div>
       <p className={styles.copy}>© 2024 잡식이라네. All rights reserved.</p>
     </div>
