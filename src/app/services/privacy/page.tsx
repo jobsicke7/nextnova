27158cc08f1f74b9018778cfaeadcf44d7098701
jobsicke <doh292929@gmail.com>
2024-12-26 18:54:51 +0900

    service add

---
+++ src/app/services/privacy/page.tsx
@@ -0,0 +1,12 @@
+
+import styles from '@/styles/main.module.css';
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <h1>
+                privacy | 이용약관 페이지 공사중...
+            </h1>
+        </div>
+    );
+}
