908149bc7b988030d072d18e812f407a8d2ee479
jobsicke <doh292929@gmail.com>
2024-12-26 13:50:54 +0900

    반응형형

--- src/components/gnb.tsx
+++ src/components/gnb.tsx
@@ -1,25 +1,40 @@
+'use client'
 // /app/components/GNB.tsx
 import React from "react";
 import styles from "../styles/gnb.module.css";
 import Link from "next/link";
-
+import Image from "next/image";
+const handleClick = () => {
+    console.log("menu clicked");
+};
 const GNB: React.FC = () => {
     return (
         <div className={styles.gnb}>
             <div className={styles.logo}>
                 <Link href="/">NEXTNOVA</Link>
             </div>
+
             <ul className={styles.menu}>
-                <li>
+                <li className={styles.link}>
                     <Link href="/">About</Link>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <Link href="/notice">Notice</Link>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <Link href="/contact">Contact</Link>
                 </li>
+
+                <Image
+                    src="/icons/menu.svg"
+                    alt="menu icon"
+                    className={styles.menuIcon}
+                    width={20}
+                    height={20}
+                    onClick={handleClick}
+                />
             </ul>
+
         </div>
     );
 };
