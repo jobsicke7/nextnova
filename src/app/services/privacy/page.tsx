4025faa700cfb32519293ed79f98d7759eaae695
jobsicke <doh292929@gmail.com>
2024-12-26 19:00:04 +0900

    fix

--- src/app/services/privacy/page.tsx
+++ src/app/services/privacy/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <h1>
-                privacy | 이용약관 페이지 공사중...
+                Privacy | 개인정보처리방침 페이지 공사중...
             </h1>
         </div>
     );
