941e6aada3b52c06ccc6cb0c82a52768da6ae4db
jobsicke <doh292929@gmail.com>
2024-12-31 13:17:42 +0900

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import NaverProvider from 'next-auth/providers/naver';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '@/lib/mongodb'; // MongoDB 연결 가져오기
+import { MongoDBAdapter } from '../../../../../node_modules/@auth/mongodb-adapter'; // MongoDB Adapter 가져오기
+import clientPromise from '../../../../lib/mongodb'; // MongoDB 연결 가져오기
 
 export const authOptions: NextAuthOptions = {
     adapter: MongoDBAdapter(clientPromise),
